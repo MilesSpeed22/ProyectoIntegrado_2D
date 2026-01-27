1368b1bf5ea7d108a33fffd6a91b3d1f46e6ab9b
MilesSpeed22 <lorperdur@gmail.com>
2026-01-27 12:10:44 +0100

    Movimiento

--- Proyecto_Intermodular_2D/Assets/Scripts/PlayerController.cs
+++ Proyecto_Intermodular_2D/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float speed;
+    bool canAttack;
+    bool isFacingRight;
+
+
+
+    Rigidbody2D PlayerRb;
+    Vector2 moveInput;
+    PlayerInput input;
+    private void Awake()
+    {
+       PlayerRb = GetComponent<Rigidbody2D>();
+       input = GetComponent<PlayerInput>(); 
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +26,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (moveInput.x > 0 && !isFacingRight) Flip();
+        //if (moveInput.x < 0 && isFacingRight) Flip();
     }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        PlayerRb.linearVelocity = new Vector2(moveInput.x * speed, moveInput.y * speed);
+    }
+
+
+    #region Input Methods
+
+
+    public void onMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void onAttack()
+    {
+
+    }
+
+    #endregion
 }
