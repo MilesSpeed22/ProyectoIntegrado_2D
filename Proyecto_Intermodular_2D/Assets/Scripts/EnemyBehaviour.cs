906d3d577cfc36af5b47582d2da516609a894749
MilesSpeed22 <lorperdur@gmail.com>
2026-01-29 10:45:54 +0100

    Deteccion d player

--- Proyecto_Intermodular_2D/Assets/Scripts/EnemyBehaviour.cs
+++ Proyecto_Intermodular_2D/Assets/Scripts/EnemyBehaviour.cs
@@ -8,6 +8,7 @@ public class EnemyBehaviour : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] bool canAttack;
     [SerializeField] GameObject attackPoint;
+    [SerializeField] bool playerDetect;
 
     private void Awake()
     {
@@ -21,7 +22,10 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FollowPlayer();
+        if (playerDetect)
+        {
+            FollowPlayer();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -31,19 +35,18 @@ public class EnemyBehaviour : MonoBehaviour
             StartCoroutine(Attack());
         }
     }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Attack"))
-        {
-            health -= 1f;
-
-            if (health <= 0)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-    }
-
+   // private void OnTriggerEnter2D(Collider2D other)
+   // {
+    //    if (other.gameObject.CompareTag("Attack"))
+    //    {
+     //       health -= 1f;
+//
+     //       if (health <= 0)
+    //        {
+    //            gameObject.SetActive(false);
+    //        }
+    //    }
+    //}
     void FollowPlayer()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
@@ -60,4 +63,21 @@ public class EnemyBehaviour : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         canAttack = true;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerDetect = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerDetect = false;
+        }
+    }
+
 }
