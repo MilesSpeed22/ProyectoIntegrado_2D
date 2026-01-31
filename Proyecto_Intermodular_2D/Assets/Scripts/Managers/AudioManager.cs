3ffcbb00b60a5f5db3d4b96cae382a27adaed185
MilesSpeed22 <lorperdur@gmail.com>
2026-01-31 23:48:26 +0100

    Ajustes

--- Proyecto_Intermodular_2D/Assets/Scripts/Managers/AudioManager.cs
+++ Proyecto_Intermodular_2D/Assets/Scripts/Managers/AudioManager.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public AudioManager Instance;
+
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    public AudioClip[] musicList;
+    public AudioClip[] sfxList;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayMusic(int musicIndex)
     {
-        
+        musicSource.clip = musicList[musicIndex];
+        musicSource.Play();
+    }
+
+    public void PlaySFX(int sfxIndex)
+    {
+        sfxSource.PlayOneShot(sfxList[sfxIndex]);
     }
 }
