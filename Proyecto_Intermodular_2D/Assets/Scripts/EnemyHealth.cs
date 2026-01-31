2ecff6e1ec527b7a0d234b37137c71d928ee05cb
MilesSpeed22 <lorperdur@gmail.com>
2026-01-31 17:01:09 +0100

    BarraDeVidaEnemigo

---
+++ Proyecto_Intermodular_2D/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("EnemyHealth")]
+    [SerializeField] float maxHealth;
+    float currentHealth;
+
+    [Header("PlayerUI")]
+    [SerializeField] RectTransform healthFill;
+    Vector3 originalScale;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        originalScale = healthFill.localScale;
+        UpdateHealthBar();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Attack"))
+        {
+            EnemyDamage(1);
+        }
+    }
+
+    void EnemyDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+        if (currentHealth <= 0) gameObject.SetActive(false);
+    }
+
+    void UpdateHealthBar()
+    {
+        float percent = currentHealth / maxHealth;
+
+        healthFill.localScale = new Vector3(originalScale.x * percent, originalScale.y, originalScale.z);
+    }
+}
